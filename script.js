b0251e6ed668a8bbe95dd519ef3e1bfe09f761b6
Abdulhakim <abdulhakimsheikh2020@gmail.com>
2021-11-29 09:21:48 -0600

    sufi

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ console.log(agaeJonas, agaeSarah);
 //assigment operators
 let x = 10 + 5;
 x += 10;
-console.log(x);*/
+console.log(x);
 const now = 2037;
 const agaJonas = now - 1991;
 const ageSarah = now - 2018;
@@ -36,4 +36,32 @@ x = y = 25 - 10 - 5;
 console.log(x, y);
 const averageAge;
 console.log(ageJonas + ageSarah) / 2;
+var ncs = 54;
+ncs = 54;
+if(ncs === 54){
+console.log("good")
+}
+else {
+    console.log("not")
+}
+const name = 'sheikh';
+if (name === jill) {
+    console.log(name);
+}
+else {
+    console.log('no name');
+}*/
+var myIndex = 0;
+carousel();
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000);
+}
 
